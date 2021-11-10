673723a1bce5c4765139964da06b81d80476bf95
Ayasnuha17 <larasnuha25@gmail.com>
2021-11-10 15:42:06 +0700

    first commit

---
+++ build/precache-manifest.a965cb0f70a62b6c45481e2cade41719.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "41cb2523019c53b35764",
+    "url": "/static/css/main.a062bdd8.chunk.css"
+  },
+  {
+    "revision": "41cb2523019c53b35764",
+    "url": "/static/js/main.41cb2523.chunk.js"
+  },
+  {
+    "revision": "df49904544551ae34e30",
+    "url": "/static/css/1.cb61e11f.chunk.css"
+  },
+  {
+    "revision": "df49904544551ae34e30",
+    "url": "/static/js/1.df499045.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a436ac81b769daed292fc9867bde969a",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ReactDex/precache-manifest.a0a9b663cd0462955f8e9d955a5082f2.js"
+  "/precache-manifest.a965cb0f70a62b6c45481e2cade41719.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ReactDex/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
